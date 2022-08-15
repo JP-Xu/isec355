9f339dbf1fc684aca19a252f1a92ba61fec99fca
JP-Xu <xu.jiam@northeastern.edu>
2022-08-14 21:44:06 -0400

    update

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
 import numpy as np
+import os
+import natsort
+import MDAnalysis
 
-
-class isec355:
+class Isec355(MDAnalysis.core.universe.Universe):
     """
     An extension class for MDAnalysis.
     This class contains several functions:
     length of side of x-y plane.
     """
     
-    def __init__(self, Universe, ts=2):
+    def __init__(self, struc_file, traj_file, ts=2):
         """ 
         input Universe: MDAnalysis Universe.
+        attributes:
+        2. lipnames: lipid names in file.
+        1. dt: time between frames.
         """
-        self.u = Universe
-        self.lipnames = [ x for x in set(self.u.residues.resnames) if "PC" in x ]
-        self.dt = ts * Universe.trajectory.dt
+        super().__init__(struc_file, traj_file)
+        self.lipnames = [ x for x in set(self.residues.resnames) if "PC" in x ]
+        self.dt = ts * self.trajectory.dt
         
     def __repr__(self):
-        resname_string = "<Residues are " + ", ".join(set(self.u.residues.resnames)) + ">\n"
-        sim_info = "<contains {} atoms, {} frames with {} ps timesteps>".format(self.u.atoms.n_atoms,
-                                                                               len(self.u.trajectory),
+        resname_string = "<Residues are " + ", ".join(set(self.residues.resnames)) + ">\n"
+        sim_info = "<contains {} atoms, {} frames with {} ps timesteps>".format(self.atoms.n_atoms,
+                                                                               len(self.trajectory),
                                                                                 self.dt)
         return resname_string + sim_info
     
     def get_box_x(self):
         """returns a list contains cell length in x direction"""
         result = []
-        for frame in self.u.trajectory:
+        for frame in self.trajectory:
             result += [frame.dimensions[0]]
         return result
     
-    def get_num_lipids(self):
+    @property
+    def num_lipids(self):
         """ return number of lipids (assuming PC) in one leaflet, assuming bilayer."""
         result = 0
         
-        for resname in self.u.residues.resnames:
+        for resname in self.residues.resnames:
             if resname in self.lipnames:
                 result += 1
         return round(result/2)
+
+    def bilayer_reshape(self):
+
+        return 
     
     def get_apl(self):
-        """ returns apl and stadard error of apl.
+        """ returns a list of apl and stadard error of apl.
         """
         boxx = self.get_box_x()
-        _, n_samples = isec355.corr_time(boxx)
+        _, n_samples = Isec355.corr_time(boxx)
         apl = np.average(boxx) ** 2 / 100 # in angstrom^2
         apl_se = np.std(boxx)/np.sqrt(n_samples)
         
         return apl, apl_se
         
-        
+    def get_thickness(self):
+        """ returns a list of thickness and standard error of thickness.
+        """
+
         
     ## Calculate autocorrelation function.
-    
+    @staticmethod
     def corr_time(array):
         """ 
         This function returns the autocorrelation function of its input.
@@ -81,11 +94,12 @@ class isec355:
         for t_prime in range(1, _length):
             c_f += [ np.average((array[:-t_prime]-_mean)*(array[t_prime:]-_mean)) / _variance]
         
-        x_zero, y_zero = isec355.first_nearest_zero(c_f)
+        x_zero, y_zero = Isec355.first_nearest_zero(c_f)
         n_samples = round(_length/x_zero)
         
         return c_f, n_samples
-                          
+     
+    @staticmethod
     def first_nearest_zero(array):
         """ returns the index and value of the first point whose y value changes sign in the array (+ -> - ).
         """
@@ -96,3 +110,49 @@ class isec355:
             
 
 
+def get_bilipid_pathes(path):
+    """ returns folder names contains results of bi-lipid bilayers under its input directory.
+    input paht: directory contains result folders.
+
+    This function works as following:
+    1. Navigates to its input folders
+    2. Searches for folders whose naming creteria is [0-9]{2-3}D[OY]D[DHLM]
+    3. Returns a list of folders
+    """
+    import regex as re
+    import natsort
+
+    os.chdir(path)
+    result = []
+    for path in os.scandir():
+        if path.is_dir() and re.match("[0-9]+D[YO]D[HDLM]", path.name):
+            result += [path.name]
+
+    pathes_for_sorting = [ re.sub('[0-9]+', "", x) + re.match('[0-9]+', x).group(0) for x in result ]
+    sorted_index = natsort.index_natsorted(pathes_for_sorting)
+    sorted_result = natsort.order_by_index( result, sorted_index)
+    return sorted_result
+
+def get_files(ext):
+    """
+    returns a list of files ends with its input in current folder with an accending order.
+    input path: path to target directory. default: current folder
+    """
+    files = os.listdir()
+    extfiles = [ file for file in files if file.endswith(ext) ]
+    extfiles = natsort.natsorted(extfiles, alg=natsort.ns.REAL)
+    return extfiles
+
+    
+
+
+
+
+if __name__ == "__main__":
+    download_folder = "/Users/jiamingxu/Downloads"
+    folders = get_bilipid_pathes(download_folder)
+    print(folders)
+    os.chdir('65DODH')
+    dcdfiles = get_files('dcd')
+    print(dcdfiles)
+    os.chdir('../')
--- run.py
+++ run.py
@@ -1,47 +1,76 @@
-import string
 from main import *
 import os
 import MDAnalysis as mda
+import natsort
+import csv
 
 # change working path
-os.chdir("/Users/jiamingxu/Downloads/65DYDM")
+# automatically and save to csv file with header:
+# apl  se  composition  gamma
+csv_header = ['APL', 'Error', 'Composition', 'Gamma'] 
+csv_data = []
 
-files = os.listdir()
-dcdfiles = [ file for file in files if file.endswith('dcd') ]
-psffiles = [ file for file in files if file.endswith('psf') ]
-gammas = [-7, 0, 7, 15]
-ts = int(input("Enter the timestep of simulation in fs/step: "))
+# Folder names
+download_folder = "/Users/jiamingxu/Downloads/"
+folders = get_bilipid_pathes(download_folder)
+apl_results = []
+## Looping all folders just scaned
 
-for gamma in gammas:
+for folder in folders:
+    print(folder)
+    os.chdir(folder)
+    
+    files = os.listdir()
+    dcdfiles = get_files('dcd')
+    psffiles = get_files('psf')
+    gammas = [-7, 0, 7, 15]
+    ts = 2 #int(input("Enter the timestep of simulation in fs/step: "))
 
-    ## Check if there is one or more than one psf file in the path.
+    for gamma in gammas:
 
-    if len(psffiles) == 1:
-        dcdfiles_in_gamma = [ file for file in dcdfiles if 'gamma' + str(gamma) in file]
-        u = mda.Universe(psffiles[0], ",".join(dcdfiles_in_gamma) )
+        dcdfiles_in_gamma = [ file for file in dcdfiles if file.startswith('gamma' + str(gamma)) ]
+        ## Check if there is one or more than one psf file in the path.
 
-    # Let user select which psf file that user wants to process.
-    elif len(psffiles) > 1:
-        string_for_query = "Enter which psf file you want to process: "
-        for i, file in enumerate(psffiles):
-            string_for_query += str(i) + " " + file + "\n"
-        input_index = input(string_for_query)
-        while True:
-            try:
-                input_index = int(input_index)
-                assert input_index in range(len(psffiles)), "Input invalid, index must be the one in above."
-                break
-            except:
-                print("Input invalid, try again.")
-            
-        u = mda.Universe(psffiles[input_index])
+        #if len(psffiles) == 1:
+        #    
+        #    ## if dcd files exist.
+        #    if dcdfiles_in_gamma != []:
+        #        u = mda.Universe(psffiles[0], dcdfiles_in_gamma)
+        #    else:
+        #        print("DCD files for gamma {} doesn't exist. Composition: {}".format(gamma, folder))
+        #
+        ## Let user select which psf file that user wants to process.
+        #elif len(psffiles) > 1:
+        #    string_for_query = "Enter which psf file you want to process: \n\n"
+        #    for i, file in enumerate(psffiles):
+        #        string_for_query += str(i) + " " + file + "\n"
+        #    input_index = input(string_for_query)
+        #    while True:
+        #        try:
+        #            input_index = int(input_index)
+        #            assert input_index in range(len(psffiles)), "Input invalid, index must be the one in above."
+        #            break
+        #        except:
+        #            print("Input invalid, try again.")
+        #        
+        #    u = mda.Universe(psffiles[input_index], dcdfiles_in_gamma)
 
-    # Raise error if no psf file exists
-    else:
-        raise  FileNotFoundError("a psf file must be placed in the same path.")
+        # Raise error if no psf file exists
+        #else:
+        #    raise  FileNotFoundError("a psf file must be placed in the same path.")
+        #u = mda.Universe("step5_input.psf", dcdfiles_in_gamma)
+        ## using isec355 to get apl and standard error.
+        lobby = Isec355("step5_input.psf", dcdfiles_in_gamma)
+        apl_result = lobby.get_apl()
+        apl_results.append(list(apl_result) + [folder] + [gamma])
 
-    ## using isec355
-    lobby = isec355(u, ts)
-    print( lobby.get_apl())
+    os.chdir("../")
 
-#u = mda.Universe()
+with open('apl.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(csv_header)
+
+    # write multiple rows
+    writer.writerows(apl_results)
